fc922bf378d255255cd9cafba243a3dd323ca125
Landdon Squires <landdonsquires@C02H91D2Q05Q.local>
2022-06-10 10:30:52 -0500

    done

--- site/module_4_solution/script.js
+++ site/module_4_solution/script.js
@@ -33,9 +33,9 @@ WARNING!!! WARNING!!!
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
     for (var i = 0; i < names.length; i++) {
-      var firstLetter = names[i].charAt(0);
+      var firstLetter = names[i].charAt(0).toLowerCase();
     
-      if (firstLetter === 'J') {
+      if (firstLetter === 'j') {
         byeSpeaker(names[i]);
       }
       else {
