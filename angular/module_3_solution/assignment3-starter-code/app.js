7addfcb617abcc5e604998fe476d35be6e9919a9
Landdon Squires <landdonsquires@C02H91D2Q05Q.local>
2022-06-22 09:52:13 -0500

    done

--- angular/module_3_solution/assignment3-starter-code/app.js
+++ angular/module_3_solution/assignment3-starter-code/app.js
@@ -2,65 +2,72 @@
     'use strict';
 
     angular.module('NarrowItDownApp', [])
-        .controller('NarrowItDownController', NarrowItDownController)
-        .service('MenuSearchService', MenuSearchService)
-        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
-        .directive('foundItems', FoundItems);
+        .controller('NarrowItDownController',NarrowItDownController)
+        .service('MenuSearchService',MenuSearchService)
+        .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com')
+        .directive('foundItems',foundItems);
 
-    function FoundItems() {
+    function foundItems() {
         var ddo = {
             restrict: 'E',
             templateUrl: 'foundItems.html',
             scope: {
                 foundItems: '<',
-                onEmpty: '<',
                 onRemove: '&'
             },
             controller: NarrowItDownController,
-            controllerAs: 'menu',
+            controllerAs: '$narrow',
             bindToController: true
         };
 
         return ddo;
     }
-
+    
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
-        var menu = this;
-        menu.shortName = '';
 
-        menu.matchedMenuItems = function(searchTerm) {
-            var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-
-            promise.then(function(items) {
-                if (items && items.length > 0) {
-                    menu.message = '';
-                    menu.found = items;
-                } else {
-                    menu.message = 'Nothing found!';
-                    menu.found = [];
-                }
-            });
-        };
-
-        menu.removeMenuItem = function(itemIndex) {
-            menu.found.splice(itemIndex, 1);
+        var $narrow = this;
+        $narrow.isShowTable = false;
+        $narrow.searchTerm = '';
+        $narrow.found = [];      
+        $narrow.error = false;
+        $narrow.getMatchedMenuItems = function(searchTerm) {
+            if (searchTerm) {
+                var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+                promise.then(function (items) {
+                    if (items.length > 0){
+                        $narrow.found = items;
+                        $narrow.isShowTable = true;
+                        $narrow.error = false;
+                    }else{
+                        $narrow.isShowTable = false;
+                        $narrow.error = true;
+                    }
+                });
+            }else{
+                $narrow.isShowTable = false;
+                $narrow.error = true;
+            }
+        }
+        $narrow.removeItem = function(index) {
+            $narrow.found.splice(index,1);
         }
     }
 
-    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    MenuSearchService.$inject = ['$http','ApiBasePath'];
     function MenuSearchService($http, ApiBasePath) {
         var service = this;
 
         service.getMatchedMenuItems = function(searchTerm) {
             return $http({
-                method: "GET",
-                url: (ApiBasePath + "/menu_items.json")
+                method: 'GET',
+                url: (ApiBasePath + '/menu_items.json')
             }).then(function(response) {
                 var foundItems = [];
 
                 for (var i = 0; i < response.data['menu_items'].length; i++) {
-                    if (searchTerm.length > 0 && response.data['menu_items'][i]['description'].toLowerCase().indexOf(searchTerm) !== -1) {
+                    if (searchTerm.length > 0 && response.data['menu_items'][i]['description']
+                    .toLowerCase().indexOf(searchTerm) !== -1) {
                         foundItems.push(response.data['menu_items'][i]);
                     }
                 }
@@ -69,4 +76,4 @@
             });
         };
     }
-})();
+}());
